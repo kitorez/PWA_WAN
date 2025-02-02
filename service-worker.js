c9c2b3472c424f6ee417c6d57fadaffee8c0ea42
Kit <kitorez@gmail.com>
2025-02-02 16:39:56 +0100

    Fix PWA 404 issue for GitHub Pages

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = "pwa-cache-v6";
 const STATIC_FILES = [
-    "./",  
-    "./index.html",
-    "./script.js",
-    "./style.css",
-    "./manifest.json"
+    "/PWA_WAN/",  
+    "/PWA_WAN/index.html",
+    "/PWA_WAN/script.js",
+    "/PWA_WAN/style.css",
+    "/PWA_WAN/manifest.json"
 ];
 
 self.addEventListener("install", (event) => {
